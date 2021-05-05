54305d620f83a848b85711e5d0512a81b15ba223
Christian Cardo <christian.cardo.dev@gmail.com>
2021-05-05 12:48:27 +0200

    test

--- src/AddMovie.jsx
+++ src/AddMovie.jsx
@@ -1,17 +1,22 @@
 import React, { Fragment, useRef } from "react";
 
 export default function AddMovie({ addNewMovie }) {
-    const inputMovie = useRef();
-    const addMovie = ()=>{
-        const newMovie = inputMovie.current.value;
-        addNewMovie(newMovie);
-        inputMovie.current.value = null;
+  const inputMovie = useRef();
+  const addMovie = () => {
+    const newMovie = inputMovie.current.value;
+    addNewMovie(newMovie);
+    inputMovie.current.value = null;
+  };
+  const keypress = (event) => {
+    if (event.key === "Enter") {
+      addMovie();
     }
+  };
   return (
     <Fragment>
       <div className="input-group mt-3">
-        <input type="text" ref={inputMovie} className="form-control" placeholder="Pelicula" />
-        <button className="btn btn-outline-secondary" type="button" onClick={addMovie}>
+        <input type="text" ref={inputMovie} className="form-control" placeholder="Pelicula" onKeyPress={keypress}/>
+        <button className="btn btn-outline-secondary" type="button" onClick={addMovie} >
           Añadir película
         </button>
       </div>
